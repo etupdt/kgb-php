6a6deda4f4c05d3ced31d633c017ef639e2ec3ea
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-10-14 14:06:24 +0200

    Style: add icons for sidemenu

--- views/admin/sidemenu.php
+++ views/admin/sidemenu.php
@@ -1,11 +1,21 @@
-    <ul class="col-6 col-lg-3 pt-5 sidemenu">
-      <li><a href="/">Dashboard</a></li>
-      <li><a href="/pays">Pays</a></li>
-      <li><a href="/planque">Planques</a></li>
-      <li><a href="/actor">Acteurs</a></li>
-      <li><a href="/specialite">Spécialités</a></li>
-      <li><a href="/typemission">Types Mission</a></li>
-      <li><a href="/statut">Statuts Mission</a></li>
-      <li><a href="/role">Roles</a></li>
-      <li><a href="/mission">Missions</a></li>
-    </ul>
+<div onclick="toggleSideMenu();">
+  <svg id="sidemenu-expand" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+  </svg>
+  <svg id="sidemenu-collapse" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+  </svg>
+</div>
+  <div class="col-12 col-md-6 col-lg-3 pt-5" class="left-menu">
+    <ul id="sidemenu" class="sidemenu">
+    <li><a href="/">Dashboard</a></li>
+    <li><a href="/pays">Pays</a></li>
+    <li><a href="/planque">Planques</a></li>
+    <li><a href="/actor">Acteurs</a></li>
+    <li><a href="/specialite">Spécialités</a></li>
+    <li><a href="/typemission">Types Mission</a></li>
+    <li><a href="/statut">Statuts Mission</a></li>
+    <li><a href="/role">Roles</a></li>
+    <li><a href="/mission">Missions</a></li>
+  </ul>
+  </div>
--- views/js/script.js
+++ views/js/script.js
@@ -1,13 +1,18 @@
 
+function toggleSideMenu () {
 
-function toggleBurger () {
-
-  const sidemenu = document.getElementById('sidemenu-ul');
+  const sidemenu = document.getElementById('sidemenu')
+  const sidemenu_expand = document.getElementById('sidemenu-expand');
+  const sidemenu_collapse = document.getElementById('sidemenu-collapse')
 
   if (sidemenu.style.display === 'none') {
     sidemenu.style.display = 'initial'
+    sidemenu_expand.style.display = 'none'
+    sidemenu_collapse.style.display = 'initial'
   } else {
     sidemenu.style.display = 'none'
+    sidemenu_expand.style.display = 'initial'
+    sidemenu_collapse.style.display = 'none'
   }
 
 }
