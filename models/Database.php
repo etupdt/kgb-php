67a11b8f19d007ccd56c98215f16baae6106889b
Denis Tavernier <denis.tavernier@gamil.com>
2024-05-18 11:52:37 +0200

    Refactor: add host and Userin env file

--- models/Database.php
+++ models/Database.php
@@ -5,8 +5,8 @@ class Database {
     // public static $host = "mysql:host=192.168.43.145;dbname=kgb";
     // public static $username = "kgb";
     // public static $password = "password";
-    public static $host = "mysql:host=192.168.1.18;dbname=kgb";
-    public static $username = "kgb";
+    public static $host = $env["DATABASE_HOST"];
+    public static $username = $env["DATABASE_USER"];
     public static $password = $env["DATABASE_PASSWORD"];
     // public static $host = "mysql:host=mysql-denist.alwaysdata.net;dbname=denist_kgb";
     // public static $username = "denist_kgb";
